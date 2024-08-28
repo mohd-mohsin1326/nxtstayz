4fb9ea75c87ef782c280c5520529db3cdae71987
mohd-mohsin1326 <mohsinmohd1326@gmail.com>
2024-08-28 13:48:56 +0000

    make changes in schema file

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,15 @@
-CREATE TABLE if not exists hotel (
-    hotelId INTEGER (Primary Key, AUTO_INCREMENT),
+CREATE TABLE IF NOT EXISTS hotel (
+    hotelId INTEGER PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(250),
     location VARCHAR(250),
     rating INTEGER
 ); 
 
-CREATE TABLE if not exists room (
-    roomId INTEGER(Primary Key, AUTO_INCREMENT),
+CREATE TABLE IF NOT EXISTS room (
+    roomId INTEGER PRIMARY KEY AUTO_INCREMENT,
     roomNumber VARCHAR(250),
     type VARCHAR(250),
     price DOUBLE,
-    hotelId INTEGER(FOREIGN KEY (hotelId) REFERENCES hotel(hotelId))
+    hotelId INTEGER,
+    FOREIGN KEY (hotelId) REFERENCES hotel(hotelId)
 );
-
